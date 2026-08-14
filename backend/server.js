11af2603563059cff16a38fdfeadfe7040e8b30f
WitcherxSN <shravan.naik30@gmail.com>
2026-08-14 18:45:23 +0530

    youtube getting ready

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 
@@ -12,6 +13,16 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("YouTube Clone API is running");
 });
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected successfully");
+  })
+  .catch((error) => {
+    console.log("MongoDB connection failed");
+    console.log(error.message);
+  });
 
 const PORT = process.env.PORT || 5000;
 
